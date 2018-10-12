3716d5b993d00e1c55ebd81cdc0b5425bf16aa1d
Arjen <az@warpnet.nl>
2018-10-12 11:40:22 +0200

    Added kdf to NaClWrapper.

--- naclencryptedfields/backends/naclwrapper.py
+++ naclencryptedfields/backends/naclwrapper.py
@@ -4,19 +4,21 @@ from .cryptowrapper import CryptoWrapper
 import base64
 
 import nacl.pwhash
-import nacl.secret
 import nacl.utils
+import nacl.secret
 
 
-# Simple wrapper around PyNaCl to standardize the initialization of the box
+# Simple wrapper around PyNaCl to standardise the initialization of the box
 # object and allow for others to extend as needed.
-class NaClWrapperException(Exception):
-	pass
-
-
 class NaClWrapper(CryptoWrapper):
-	def __init__(self, keydata, *args, **kwargs):
-		self.box = nacl.secret.SecretBox(keydata)
+	salt = 'tHeQ/aaj8e4Z9Jj33+xZOQ=='
+
+	def __init__(self, keydata, apply_kdf=False, *args, **kwargs):
+		key = base64.b64decode(keydata)
+		if apply_kdf or len(keydata) != nacl.secret.SecretBox.KEY_SIZE:
+			key = NaClWrapper.kdf(key)
+
+		self.box = nacl.secret.SecretBox(key)
 
 	def encrypt(self, plaintext):
 		ciphertext = self.box.encrypt(plaintext.encode())
@@ -28,4 +30,12 @@ class NaClWrapper(CryptoWrapper):
 
 	@staticmethod
 	def createKey():
-		return nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
+		return base64.b64encode(nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE))
+
+	@staticmethod
+	def kdf(password):
+		return nacl.pwhash.argon2i.kdf(nacl.secret.SecretBox.KEY_SIZE,
+		                               password.encode(),
+		                               base64.b64decode(NaClWrapper.salt),
+		                               opslimit=nacl.pwhash.argon2i.OPSLIMIT_MIN,
+		                               memlimit=nacl.pwhash.argon2i.MEMLIMIT_MIN)
